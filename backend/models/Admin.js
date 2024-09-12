35e66bf1861e4b56733f29e1e844ad4cc6314383
rajkanugala7 <rajkanugala7@gmail.com>
2024-09-12 12:15:58 +0530

    up

---
+++ backend/models/Admin.js
@@ -0,0 +1,36 @@
+const mongoose =require("mongoose");
+const Schema=mongoose.Schema;
+const bcrypt=require("bcryptjs");
+const AdminSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    }
+})
+
+
+AdminSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+            next();
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        next();
+    }
+});
+
+const Admin=new mongoose.model("Admin",AdminSchema);
+
+module.exports=Admin;

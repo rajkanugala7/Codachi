157c5331be6f8bee75dd100f53bc0eab4c8b3c78
rajkanugala7 <rajkanugala7@gmail.com>
2024-09-29 00:24:40 +0530

    up

---
+++ frontend/frontendcodachi/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CompilerPage from './components/CompilerPage';
+
+function App() {
+  return (
+    <div className="App">
+      <CompilerPage/>
+    </div>
+  );
+}
+
+export default App;
